2af3e360ac5a603998571709d15dd9926213bd72
Alibek Ismailov <alibek.ismailov@bcc.kz>
2024-08-11 12:38:56 +0500

    Sun 08/11/2024 - 12:38:56.08

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/Epic55/ecommerce/internal/controllers"
+	"github.com/Epic55/ecommerce/internal/middleware"
+	"github.com/Epic55/ecommerce/internal/repository"
+	"github.com/Epic55/ecommerce/internal/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	app := controllers.NewApplication(repository.ProductData(repository.Client, "Products"), repository.UserData(repository.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/listcart", controllers.GetItemFromCart())
+	router.POST("/addaddress", controllers.AddAddress())
+	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	router.GET("/deleteaddresses", controllers.DeleteAddress())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+	log.Fatal(router.Run("localhost:" + port))
+}
